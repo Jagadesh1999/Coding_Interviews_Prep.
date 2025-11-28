0c008ef4f980c65279d72abe58236407e06a4ab1
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-11-28 16:01:59 +0530

    Update 1_Foundational Problems.cpp

--- Binary Search/1_Foundational Problems.cpp
+++ Binary Search/1_Foundational Problems.cpp
@@ -115,21 +115,7 @@ int main()
 // 2. Upper-Bound 
 // S. Inside the check(mid) function --> if (nums[mid] > x), return 1
 
-// 3. Minimum in Rotated Sorted Array (The elements are distinct and the rotations are anti-clockwise)
-// S. Inside the check(mid) function --> if (nums[mid] <= nums[nums.size()-1]), return 1 
-   // Think why the comparison with nums[0] might disturb the monotonic property when the input array is perfectly sorted.
-   // Similar Problem - Find out the total number of rotations in the rotated sorted array.
-   // The simple observation and the code change needed if the rotations were clockwise.
-      // Still, the minimum element stays the same.
-      // No further code change is needed. 
-      // The only formula needed is, Rotations = nums.size() - nums[sol].
-      
-// 4. Index of the peak element in a bitonic array - (Bitonic array - value increases strictly till one point and then it starts strictly decreasing)
-// S. Inside the check(mid) function --> if ((mid < nums.size()-1) && (nums[mid] > nums[mid+1]), return 1
-                                //   --> if (mid == nums.size()-1) return 1 
-                                //   --> return 0.
-                                
-// 6. Search Insert Position
+// 3. Search Insert Position
 // Samples :
    // Input: nums = [1, 3, 5, 6], target = 5. Output: 2. Explanation: The target value 5 is found at index 2 in the sorted array. Hence, the function returns 2.
    // Input: nums = [1, 3, 5, 6], target = 2. Output: 1. Explanation: The target value 2 is not found in the array. However, it should be inserted at index 1 to maintain the sorted order of the array.
@@ -137,16 +123,56 @@ int main()
       // --> if (nums[mid] >= x), return 1
       // --> if it returns 1, then inside the while loop, check if (nums[mid] == x), if yes, sol = mid; break;
       // -->                                                                         if no, sol = mid; high = mid - 1
-      
-// 7. Floor and ceil of the target in the sorted array.
+
+// 4. Floor and ceil of the target in the sorted array.
 // S. Inside the check(mid) function : if(nums[mid] >= target), return 1
       // --> sol will store the ceil value at the exit of the while loop.
       // --> If ceil == target, then floor is also the same value as ceil, else floor is the value of the element previous to that of the floor.
-      
-// 8. Last occurence of the given target in the sorted array. (Consider a small change we are finding the index of the last 0, instead of the first one)
+
+// 5. Last occurence of the given target in the sorted array. (Consider a small change we are finding the index of the last 0, instead of the first one)
 // S. Inside the check(mid) function : if(nums[mid] == x), return 0.
       // Inside the loop, If check(mid) == 0, sol = mid; low = mid + 1
       // Else, check if(nums[mid] > target), YES - high = mid - 1. NO - low = mid + 1.
+
+// 6. Count the occurences of a given number in the sorted array.
+// S. The problem needs to be solved using 2 parts, the first occurence part and the last occurence part. (L - F + 1) is the total occurence.
+
+// 7. Search in rotated sorted array (Distinct elements Version)
+// Tips : In the rotated sorted array with distinct elements, for any element mid, one half is sorted and the other half is not sorted.
+// S. First check if nums(mid) is the target and return accordingly.
+      // Inside the check(mid) function :
+         // 1. Check if left half is sorted (nums[low] <= nums[mid]) and if yes, search for the target in that half (nums[low] <= target < nums[mid]).
+               // If the element lies in that range, do high = mid - 1, Else do low = mid + 1.
+         // 2. Else if the left half is not sorted, search for the target in the right half (nums[mid] <= target < nums[high]). 
+               // If the element lies in that range, do low = mid + 1. else do high = mid - 1.
+
+// 8. Search in rotated sorted array (Duplicates elements Version)
+// S. First check if nums(mid) is the target and return accordingly.
+      // Inside the check(mid) function :
+         // 1. Check if left half is sorted (nums[low] < nums[mid]) and if yes, search for the target in that half (nums[low] <= target < nums[mid]).
+               // If the element lies in that range, do high = mid - 1, Else do low = mid + 1.
+         // 2. Else if the left half is not sorted, search for the target in the right half (nums[mid] <= target < nums[high]). 
+               // If the element lies in that range, do low = mid + 1. else do high = mid - 1.
+         // 3. Else, shrink at both the ends. do low++ and high--.
+
+// 9. Minimum in Rotated Sorted Array (The elements are distinct and the rotations are anti-clockwise)
+// S. Inside the check(mid) function --> if (nums[mid] <= nums[nums.size()-1]), return 1 
+   // Think why the comparison with nums[0] might disturb the monotonic property when the input array is perfectly sorted.
+   // Similar Problem - Find out the total number of rotations in the rotated sorted array.
+   // The simple observation and the code change needed if the rotations were clockwise.
+      // Still, the minimum element stays the same.
+      // No further code change is needed. 
+      // The only formula needed is, Rotations = nums.size() - nums[sol].
+
+// 10. Every element occurs twice except the single element. Find that single Element in a Sorted Array.
+// Tips : For any nums[mid] at even index, nums[mid] == nums[mid + 1]. So, for any mid calculated, if its odd, do mid--;
+// S. Inside the check(mid) function -->
+      
+// 11. Index of the peak element in a bitonic array - (Bitonic array - value increases strictly till one point and then it starts strictly decreasing)
+// S. Inside the check(mid) function --> if ((mid < nums.size()-1) && (nums[mid] > nums[mid+1]), return 1
+                                //   --> if (mid == nums.size()-1) return 1 
+                                //   --> return 0.
+      
                                 
 // Some unique problems that does not follow monotonicity and technically not a BS problem but needs a binary search like logic :
 // ------------------------------------------------------------------------------------------------------------------------------
